a738a2cb1596b49570ec8013717185f0f8d8425e
Jm1211 <110876203+Jm1211@users.noreply.github.com>
2022-12-16 20:14:40 -0700

    add seeds

---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+USE employee_db;
+
+INSERT INTO departments (name)
+VALUES
+("Sales"),
+("Engineering"),
+("Finance"),
+("Legal");
+
+INSERT INTO roles (title,salary,department_id)
+VALUES
+('Sales Lead', 100000, 1),
+('Salesperson', 80000, 1),
+('Lead Engineer', 150000, 2),
+('Software Engineer', 120000, 2),
+('Accountant', 125000, 3),
+('Legal Team Lead', 250000, 4),
+('Lawyer', 190000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+("Harry", "Potter", 1),
+("Ronald", "Weasley", 2),
+("Hermonie", "Granger", 3),
+("Draco", "Malfoy", 4),
+("Severus", "Snape", 5),
+("Sirius", "Black", 6),
+("Rubeus", "Hagrid", 7),
+("Minerva", "Mcgonagall", 5),
+("Luna", "Lovegood", 4);
--- server.js
+++ server.js
@@ -1,5 +1,23 @@
+const db = require('./db/connection');
+const startInquirer = require('.lib/departments')
 const express = require('express');
-const mysql = require('mysql2');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use((req,res) => {
+    res.status(404).end();
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('connected.');
+    app.listen(PORT, () => {
+        console.log(`Server now running on port ${PORT}`);
+        startInquirer();
+    });
+});
